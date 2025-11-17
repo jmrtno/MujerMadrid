2e82dd50e4a6fbf01bfbde6a514f7a7716b6a218
jmrtno <javiermartino96@gmail.com>
2025-11-17 15:54:23 +0100

    Delete some comments. Manage home screen when launch from ipad

--- CercaDeTi/Sources/Presentation/Navigation/IncomingNavigation.swift
+++ CercaDeTi/Sources/Presentation/Navigation/IncomingNavigation.swift
@@ -10,14 +10,10 @@
 import Foundation
 import FNavigation
 
-// Una enumeración que representa las diferentes opciones de navegación.
-/// - Enumera las posibles navegaciones (screenOne, screenTwo).
-/// - presentationType: Define cómo se debe presentar cada pantalla (modal o push).
 public enum IncomingNavigation: NavigationInfo {
     case home
     case detail(centerId: String)
 
-    /// Determina el tipo de presentación para cada opción de navegación.
     public var presentationType: PresentationType {
         switch self {
         case .home:
