22a12801bf58a8acb4acfd9a7b3d7437b35a0308
jmrtno <javiermartino96@gmail.com>
2025-11-18 23:39:14 +0100

    missing header folder

---
+++ MujerMadrid/Sources/Presentation/Home/Sections/Header/WomenCarePointHomeHeaderSectionViewModelContract.swift
@@ -0,0 +1,21 @@
+//
+//  WomenCarePointHomeHeaderSectionViewModelContract.swift
+//  MujerMadrid
+//
+//  Created by Javier Martin on 18/11/25.
+//
+
+
+//
+//  WomenCarePointHomeHeaderSectionViewModelContract.swift
+//  MujerMadrid
+//
+//  Created by Javier Martin on 13/10/25.
+//
+
+import Combine
+
+public protocol WomenCarePointHomeHeaderSectionViewModelContract {
+    // MARK: - Inputs
+    func didTapToggle()
+}
