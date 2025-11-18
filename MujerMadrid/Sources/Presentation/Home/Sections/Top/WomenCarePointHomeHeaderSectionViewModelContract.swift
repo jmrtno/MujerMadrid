c88e2e6ddae7988aa18fd55d0b258a5f27a4fd06
jmrtno <javiermartino96@gmail.com>
2025-11-18 21:29:36 +0100

    Add missing folder 2

---
+++ MujerMadrid/Sources/Presentation/Home/Sections/Top/WomenCarePointHomeHeaderSectionViewModelContract.swift
@@ -0,0 +1,13 @@
+//
+//  WomenCarePointHomeHeaderSectionViewModelContract.swift
+//  MujerMadrid
+//
+//  Created by Javier Martin on 13/10/25.
+//
+
+import Combine
+
+public protocol WomenCarePointHomeHeaderSectionViewModelContract {
+    // MARK: - Inputs
+    func didTapToggle()
+}
