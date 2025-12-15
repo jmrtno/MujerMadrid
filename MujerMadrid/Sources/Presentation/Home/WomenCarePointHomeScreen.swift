e1df8a65d4876f561f1bdbdb63c80d001cd81423
JaviDev <javi.apps.developer@gmail.com>
2025-12-15 23:49:39 +0100

    Hide or show footer shadow when scroll bottom reached

--- MujerMadrid/Sources/Presentation/Home/WomenCarePointHomeScreen.swift
+++ MujerMadrid/Sources/Presentation/Home/WomenCarePointHomeScreen.swift
@@ -22,9 +22,12 @@ struct WomenCarePointHomeScreen<Top: View,
     private let error: Error
     private let viewModel: WomenCarePointHomeViewModelContract
 
-    @SwiftUI.State private var viewSize: CGSize = .zero
     @SwiftUI.State private var showError = false
     @SwiftUI.State private var showLoader = false
+    @SwiftUI.State private var didReachBottom = false
+    @SwiftUI.State private var scrollViewHeight: CGFloat = 0
+
+    private let bottomDetectionMargin: CGFloat = 20
 
     /// Init of ``WomenCarePointHomeScreen`
     /// - Parameters:
@@ -53,9 +56,55 @@ struct WomenCarePointHomeScreen<Top: View,
             BackgroundView()
             VStack {
                 top
-                content
-                bottom
+                ScrollView {
+                    content
+                        .background(
+                            GeometryReader { proxy in
+                                Color.clear
+                                    .preference(
+                                        key: BottomSentinelMaxYPreferenceKey.self,
+                                        value: proxy.frame(in: .named("WomenCarePointScroll")).maxY
+                                    )
+                            }
+                        )
+                }
+                .coordinateSpace(name: "WomenCarePointScroll")
+                .background(
+                    GeometryReader { geo in
+                        Color.clear
+                            .onAppear {
+                                scrollViewHeight = geo.size.height
+                            }
+                            .onChange(of: geo.size.height) { _, new in
+                                scrollViewHeight = new
+                            }
+                    }
+                )
+                .onPreferenceChange(BottomSentinelMaxYPreferenceKey.self) { maxY in
+                    let margin = bottomDetectionMargin
+                    let reached = maxY <= (scrollViewHeight + margin)
+                    if reached != didReachBottom {
+                        didReachBottom = reached
+                    }
+                }
+                ZStack(alignment: .top) {
+                    // Footer shadow
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black.opacity(didReachBottom ? 0 : 0.1),
+                            .clear
+                        ]),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    )
+                    .frame(height: 12)
+                    .offset(y: -18)
+                    bottom
+                        .ignoresSafeArea(.all)
+                }
             }
+            .frame(maxWidth: .infinity)
+            .background(.white)
             .hiddenOrRemoved(showError, remove: true)
             if showError {
                 error
@@ -73,6 +122,13 @@ struct WomenCarePointHomeScreen<Top: View,
         .onReceive(viewModel.loaderPublisher) {
             showLoader = $0
         }
+    }
+}
+
+private struct BottomSentinelMaxYPreferenceKey: PreferenceKey {
+    static let defaultValue: CGFloat = .infinity
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = min(value, nextValue())
     }
 }
 
